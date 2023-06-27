8c27b71be2b6f03971e8eb963adde6044cb68018
ricardoisoton <48535227+ricardoisoton@users.noreply.github.com>
2023-06-27 11:05:36 -0300

    Add files via upload

--- grafo.js
+++ grafo.js
@@ -1,5 +1,6 @@
+// Função para colorir o grafo
 function colorGraph(numVertices, connections) {
-  const colors = ['vermelho', 'azul', 'verde']; // Lista de cores disponíveis
+  const colors = ['red', 'blue', 'green']; // Lista de cores disponíveis
   const coloredVertices = {}; // Armazena as cores atribuídas aos vértices
 
   // Itera sobre cada vértice
@@ -20,6 +21,7 @@ function colorGraph(numVertices, connections) {
         break;
       }
     }
+
     // Atribui a cor ao vértice que não está sendo usada pelos vizinhos
     for (let color of colors) {
       if (!neighborColors.includes(color)) {
@@ -31,19 +33,19 @@ function colorGraph(numVertices, connections) {
   return coloredVertices;
 }
 
-  // Definição do número de vértices e conexões
-  const numVertices = 7;
-  const connections = [
-    { vertex: 'A', neighbors: ['B'] },
-    { vertex: 'B', neighbors: ['C', 'E'] },
-    { vertex: 'C', neighbors: ['D', 'E'] },
-    { vertex: 'D', neighbors: ['C'] },
-    { vertex: 'E', neighbors: ['B', 'C', 'F', 'G'] },
-    { vertex: 'F', neighbors: ['E', 'G'] },
-    { vertex: 'G', neighbors: ['E', 'F'] }
-  ];
+// Definição do número de vértices e conexões
+const numVertices = 7;
+const connections = [
+  { vertex: 'A', neighbors: ['B'] },
+  { vertex: 'B', neighbors: ['C', 'E'] },
+  { vertex: 'C', neighbors: ['D', 'E'] },
+  { vertex: 'D', neighbors: ['C'] },
+  { vertex: 'E', neighbors: ['B', 'C', 'F', 'G'] },
+  { vertex: 'F', neighbors: ['E', 'G'] },
+  { vertex: 'G', neighbors: ['E', 'F'] }
+];
 
-  // Chamada da função e obtenção das cores atribuídas aos vértices
+// Chamada da função e obtenção das cores atribuídas aos vértices
 const coloredVertices = colorGraph(numVertices, connections);
 
 // Atualizar as cores dos vértices com base nos resultados
@@ -74,14 +76,13 @@ vertexElements.forEach((vertexElement) => {
   vertexElement.style.top = position.y + 'px';
 });
 
-  // Função para desenhar as arestas de conexão
+// Função para desenhar as arestas de conexão
 function drawConnections(connections) {
   const svg = document.getElementById('connections-svg');
 
   for (let connection of connections) {
     const { vertex, neighbors } = connection;
     const startVertex = document.getElementById(`vertex-${vertex}`);
-
     const startX = startVertex.offsetLeft + startVertex.offsetWidth / 2;
     const startY = startVertex.offsetTop + startVertex.offsetHeight / 2;
 
